1d57bb5d36941f19cb2453654aeac5a7d5b9e072
MrFast <mrfastkrunker@gmail.com>
2024-04-21 16:22:49 -0400

    Add recent scans

--- app/src/main/java/com/example/nutritionalbarcodescanner/menuPages/HomeFragment.kt
+++ app/src/main/java/com/example/nutritionalbarcodescanner/menuPages/HomeFragment.kt
@@ -4,8 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.example.nutritionalbarcodescanner.MainActivity
 import com.example.nutritionalbarcodescanner.R
+import org.json.JSONObject
 
 class HomeFragment : Fragment() {
 
@@ -13,7 +19,36 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val recentScannedProducts = requireActivity().getSharedPreferences("recentScannedProducts", AppCompatActivity.MODE_PRIVATE)
+        val productCache = requireActivity().getSharedPreferences("ProductIdFetchCache", AppCompatActivity.MODE_PRIVATE)
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_home, container, false)
+        for (recentProduct in recentScannedProducts.all) {
+            for (product in productCache.all) {
+                if(product.value.toString().contains(recentProduct.key)) {
+                    // Inflate the new CardView layout
+                    val newCardViewLayout = inflater.inflate(R.layout.recent_scan_entry_layout, null, false)
+
+                    // Find ImageView in the new CardView layout
+                    val recentScanProductImage = newCardViewLayout.findViewById<ImageView>(R.id.productImage)
+
+                    // Load image into ImageView using Glide
+                    Glide.with(this@HomeFragment).load(recentProduct.value).into(recentScanProductImage)
+
+                    // Find the LinearLayout to add the new CardView
+                    val recentScansContainer = rootView.findViewById<LinearLayout>(R.id.recentScansContainer)
+
+                    newCardViewLayout.setOnClickListener {
+                        (requireActivity() as MainActivity).openProductInfoFragment(JSONObject(product.value as String))
+                    }
+
+                    // Add the new CardView to the LinearLayout
+                    recentScansContainer.addView(newCardViewLayout)
+                }
+            }
+        }
+
+        return rootView
     }
 }
